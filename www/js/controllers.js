f6cb8727ee76937fb558f7a2d6ff3582b24e64bd
Brian Boyko <brian.boyko@gmail.com>
2015-11-07 12:51:15 -0600

    angular works as expected, onto database storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,38 +4,53 @@ angular.module('starter.controllers', [])
 .controller('QuizCtrl', function($scope, $rootScope, Quiz) {
 
 // Hmm, for such a small app, I don't mind using $rootScope instead of
-// setting up a huge factory. 
-  // $rootScope.firstname = '';
-  // $rootScope.lastname = '';
-  // $rootScope.email = '';
+// setting up a huge factory. This controller might also be abstracted out
+// a bit, so there's less logic in it.  
+
   $rootScope.user = {}
 
   $scope.questions = Quiz.all();
 
   $rootScope.responses = {
-    'q1': {},
-    'q2': {},
-    'q3': {},
-    'q4': {},
-    'q5': {},
-    'q6': {},
-    'q7': {},
-    'q8': {}
+    'q1': undefined,
+    'q2': undefined,
+    'q3': undefined,
+    'q4': undefined,
+    'q5': undefined,
+    'q6': undefined,
+    'q7': undefined,
+    'q8': undefined
   }
 
   $scope.inputUser = function(){
     $rootScope.first = $scope.firstname;
     $rootScope.last = $scope.lastname;
     $rootScope.email = $scope.emailAddress;
-     console.log( $rootScope.firstname, $rootScope.lastname, $rootScope.email);
   }
 
-  $scope.selectChange = function(answer, question) {
-    console.log("Question", question.prompt, "Answer", answer.text, "value:", answer.value);
-    console.log("responses:", JSON.stringify($scope.responses));
-    console.log("Value Changed");
-    };
+
+  $scope.gotEmail = function(){
+    if ($rootScope.email === undefined || $rootScope.email === '' || $rootScope.email.indexOf('@') === -1){
+      return false;
+    }
+    return true; 
+  }  
+
+  $scope.gotQuestions = function(){
+    for (var key in $rootScope.responses){
+      if ($rootScope.responses[key] === undefined){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  $scope.isReady = function(){
+    return ($scope.gotEmail() && $scope.gotQuestions); 
+  }
+
+
 
 })
 
-.controller('EmailCtrl')
+
